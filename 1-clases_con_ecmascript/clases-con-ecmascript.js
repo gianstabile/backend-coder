7e54e9f4018f380648206947b0b8e848fdf360d7
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-02-18 13:45:56 -0300

    Se agregó validación de campos

--- 1-clases_con_ecmascript/clases-con-ecmascript.js
+++ 1-clases_con_ecmascript/clases-con-ecmascript.js
@@ -4,20 +4,29 @@ class ProductManager {
   };
 
   //metodo para generar nuevos productos
-  addProduct = (title, description, price, thumbnail, stock) => {
+  addProduct = (title, description, price, thumbnail, code, stock) => {
     const product = {
+      id: this.products.length + 1,
       title,
       description,
       price,
       thumbnail,
-      code: this.products.length + 1,
-      stock: stock ?? 50,
+      code,
+      stock
+    }
+
+    // validacion de campos requeridos
+    for (const p in product) {
+      if (product[p] === undefined) {
+        console.log("Incomplete fields!")
+        return;
+      }
     }
 
     //validacion de existencias
-    const productId = this.products.find(element => element.code === product.code)
+    const productCode = this.products.find(e => e.code === product.code)
 
-    if (!productId) {
+    if (!productCode) {
       this.products.push(product);
       console.log("Product add succefully.")
     } else {
@@ -31,18 +40,21 @@ class ProductManager {
   }
 
   getProductsById = (productId) => {
-    if (!this.products.includes(this.products[productId])) {
+    const productExist = this.products.find(element => element.id === productId)
+
+    if (!productExist) {
       console.log("Not found.")
     } else {
-      console.log("It's found.")
+      console.log(productExist)
     }
   };
 };
 
 const product1 = new ProductManager();
 
-product1.addProduct("Oreo", "Galletitas dulces", 280, "http://www.google.com/imagen.jpg", 23);
-product1.addProduct("Coca", "Gaseosa de cola", 400, "http://www.google.com/imagen.jpg");
+product1.addProduct("Quilmes", "Cerveza rubia", 299, "http://www.google.com/imagen.jpg", 0323, 35);
+product1.addProduct("Coca", "Gaseosa de cola", 400, "http://www.google.com/imagen.jpg", 0345);
+product1.addProduct("Agua", "Agua mineral", 200, "http://www.google.com/imagen.jpg", 0323, 44);
 
 product1.getProducts();
 product1.getProductsById(1);
