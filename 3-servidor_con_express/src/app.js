de53f227fbc954fb82bb1f4d0e7984b3973c1b04
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-03-04 13:46:18 -0300

    agregué el filter por query de productos

--- 3-servidor_con_express/src/app.js
+++ 3-servidor_con_express/src/app.js
@@ -5,33 +5,39 @@ const app = express();
 let port = 8080;
 let manager = new ProductManager("./files/products.json");
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`<h3>Welcome to Express server!</h3>`);
 });
 
+//endpint que muestra todos los productos
 app.get("/products", async (req, res) => {
   const allProducts = await manager.getProducts();
   res.json(allProducts);
 });
 
-// app.get('/users',function(req,res){
-//   req.query.limit = req.query.limit || 10;
-//   const limit = parseInt(req.query.limit,10);
-//   if(Number.isNaN(limit)){
-//    return res.status(400).end();
-//    //만약에 NAN 이면 400이란 코드를 내보내고 종료해라.
-//   }
-//   res.json(users.slice(0,limit));
-//  });
+//endpoint que filtra por query por cantidad solicitada
+//no me permitía partir de products?limit , por eso agregué una "q" a la url
+app.get("/products/q", async (req, res) => {
+  const limit = req.query.limit;
+  const products = await manager.getProducts();
 
+  if (Number.isNaN(limit)) {
+    return res.send({ Error: "Not found." });
+  }
+
+  const filterProducts = await products.slice(0, limit);
+  res.send(filterProducts);
+});
+
+//endpoint que filtra por params por Id de producto
 app.get("/products/:pid", async (req, res) => {
-  let id = req.params.pid;
-  const product = await manager.getProductsById(Number(id));
-  console.log(product);
+  const { pid } = req.params;
+  const product = await manager.getProductsById(Number(pid));
 
-  if (!product) return res.send({ error: "Not found." });
+  if (!product) return res.send({ Error: "Product not found." });
   res.send(product);
 });
 
