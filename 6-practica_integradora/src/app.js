9746c3dd895f99474678233a6febc2bd56f7ea12
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-04-12 09:08:36 -0300

    Add files via upload

--- 6-practica_integradora/src/app.js
+++ 6-practica_integradora/src/app.js
@@ -9,7 +9,9 @@ import messagesRouter from "./routes/messages.router.js";
 import cartsRouter from "./routes/carts.router.js";
 import viewsRouter from "./routes/views.router.js";
 import socket from "./socket.js";
+import path from "path";
 import __dirname from "./utils.js";
+import { productModel } from "./dao/models/products.model.js";
 
 const app = express();
 
@@ -20,14 +22,19 @@ const dbName = process.env.DB_NAME;
 const port = process.env.PORT || 8080;
 
 //mongoose
-mongoose.connect(
-  `mongodb+srv://${dbUser}:${dbPassword}@practica-integradora.d7s7guz.mongodb.net/${dbName}?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  console.error("Error connecting to MongoDB:", err);
-});
+const environment = async () => {
+  await mongoose
+    .connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@practica-integradora.d7s7guz.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+    });
+};
 
 //socket.io
 const httpServer = app.listen(port, (err) => {
@@ -38,17 +45,17 @@ socket.connect(httpServer);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 
 //handlebars
-app.set("views", `${__dirname}/views`);
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", ".hbs");
 app.engine(
   "hbs",
   handlebars.engine({
     defaultLayout: "main",
     extname: ".hbs",
-    layoutsDir: `${__dirname}/views/layout`,
+    layoutsDir: path.join(app.get("views"), "layout"),
   })
 );
 
@@ -59,3 +66,5 @@ app.use("/api/carts", cartsRouter);
 
 app.get("/", viewsRouter);
 app.get("/realtimeproducts", viewsRouter);
+
+environment();
