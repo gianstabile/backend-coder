37569040df87edc27364c161c82888520474921d
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-05-24 08:04:11 -0300

    Add files via upload

--- 11-reestructura_de_servidor/src/config/db.js
+++ 11-reestructura_de_servidor/src/config/db.js
@@ -5,11 +5,9 @@ const { database } = config;
 const db = {
   connect: async () => {
     try {
-      await mongoose
-        .connect(database.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-          console.log("Connected to MongoDB");
-        });
+      await mongoose.connect(database.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        console.log("Connected to MongoDB");
+      });
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
     }
