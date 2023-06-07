1d68a26d0dc0f262e44b548fbbbf6e4350dab30d
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-06-07 20:11:57 -0300

    Add files via upload

--- 12-tercera_pre_entrega/src/dao/models/order.model.js
+++ 12-tercera_pre_entrega/src/dao/models/order.model.js
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
-import { v4 as uuidv4 } from "uuid";
 
 const collection = "Orders";
 const orderSchema = new mongoose.Schema({
-  code: { type: String, default: uuidv4, unique: true, required: true },
-  purchase_datetime: { type: Date, default: Date.now },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
-  amount: { type: Number, required: true },
-  purchaser: { type: String, required: true },
+  code: {
+    type: String,
+    unique: true,
+  },
+  purchase_datetime: {
+    type: String,
+  },
+  amount: {
+    type: Number,
+  },
+  purchaser: {
+    type: String,
+  },
 });
 
 const orderModel = mongoose.model(collection, orderSchema);
