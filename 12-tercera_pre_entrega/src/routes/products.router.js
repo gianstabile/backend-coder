3ce34de4485ea9ef95ab6b33e5b06073ac0cb94e
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-06-07 20:13:54 -0300

    Add files via upload

--- 12-tercera_pre_entrega/src/routes/products.router.js
+++ 12-tercera_pre_entrega/src/routes/products.router.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { uploader } from "../utils.js";
 import ProductController from "../controllers/products.controller.js";
+import { authorizeRole } from "../middlewares/auth.js";
 
 const router = Router();
 const productController = new ProductController();
 
 router.get("/", productController.getProducts);
 router.get("/:pid", productController.getProductById);
-router.post("/", uploader.array("thumbnails", 5), productController.createProduct);
-router.put("/:pid", productController.updateProduct);
-router.delete("/:pid", productController.deleteProduct);
+router.post("/", uploader.array("thumbnails", 5), authorizeRole(["admin"]), productController.createProduct);
+router.put("/:pid", authorizeRole(["admin"]), productController.updateProduct);
+router.delete("/:pid", authorizeRole(["admin"]), productController.deleteProduct);
 
 export default router;
