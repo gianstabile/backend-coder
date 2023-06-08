f4f086c9e9c547cd263a506799f51f6270579251
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-06-08 07:59:04 -0300

    Add files via upload

--- 12-tercera_pre_entrega/src/routes/views.router.js
+++ 12-tercera_pre_entrega/src/routes/views.router.js
@@ -8,13 +8,13 @@ const viewsController = new ViewsController();
 
 router.get("/products", viewsController.getProducts);
 router.get("/product/:pid", viewsController.getProductById);
-router.get("/cart", viewsController.getCart);
+router.get("/cart", authorizeRole(["user"]), viewsController.getCart);
 router.get("/login", checkLogged, viewsController.getLogin);
 router.get("/register", checkLogged, viewsController.getRegister);
-router.get("/profile", authorizeRole(["user"]), checkLogin, viewsController.getProfile);
+router.get("/profile", checkLogin, viewsController.getProfile);
 router.get("/logout", viewsController.logout);
 router.get("/recovery", viewsController.getRecovery);
 router.get("/current", viewsController.getCurrentUser);
-router.get("/purchase", viewsController.purchase);
+router.get("/purchase", authorizeRole(["user"]), viewsController.purchase);
 
 export default router;
