0daa5c3bb1b6827ec2dab789fc278f3ea3a4ecda
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-06-12 09:51:25 -0300

    Delete errorHandlerMiddleware.js

--- 13-mocking_manejo_de_errores/src/middlewares/errorHandlerMiddleware.js
+++
@@ -1,8 +0,0 @@
-export default function errorHandlerMiddleware(err, req, res, next) {
-  if (err.code && err.message) {
-    res.status(500).json({ error: err.message });
-  } else {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
-  }
-}
