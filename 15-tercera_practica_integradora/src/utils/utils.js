17a2addb6bcdc64cb551f02666133ad2142951fd
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-07-05 09:47:43 -0300

    Add files via upload

--- 15-tercera_practica_integradora/src/utils/utils.js
+++ 15-tercera_practica_integradora/src/utils/utils.js
@@ -7,7 +7,7 @@ import bcrypt from "bcrypt";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// multer
+// Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, `${__dirname}/public/images`);
@@ -17,12 +17,11 @@ const storage = multer.diskStorage({
   },
 });
 
-// bcrypt
+export const uploader = multer({ storage });
+
+// Bcrypt
 export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password);
-
-export const uploader = multer({ storage });
-export default __dirname;
 
 export const calculateExpirationDate = () => {
   const now = new Date();
@@ -33,3 +32,5 @@ export const calculateExpirationDate = () => {
 export const generateUniqueToken = () => {
   return uuid();
 };
+
+export default __dirname;
