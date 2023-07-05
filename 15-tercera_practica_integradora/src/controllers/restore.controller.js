14fd79760dd2b2e95c9fe47478ed6e4e82538274
Gianni <102770885+gianstabile@users.noreply.github.com>
2023-07-05 09:46:31 -0300

    Add files via upload

--- 15-tercera_practica_integradora/src/controllers/restore.controller.js
+++ 15-tercera_practica_integradora/src/controllers/restore.controller.js
@@ -1,25 +1,45 @@
 import { restoreService } from "./../services/restore.service.js";
+import { logger } from "../utils/logger.js";
+import CustomError from "../errors/customError.js";
 
-export async function restore(req, res) {
+export async function restore(req, res, next) {
   try {
     const { email } = req.body;
 
-    await restoreService.createRestore(email);
+    const result = await restoreService.createRestore(email);
 
-    return res.status(200).send("Password reset email sent successfully.");
+    logger.info("Password reset email sent successfully.");
+
+    res.status(200).send(result);
   } catch (error) {
-    return res.status(500).send(error.message);
+    logger.error("Failed to send the password reset email.", error);
+    next(
+      new CustomError.createCustomError({
+        name: errorsName.RESTORE_ERROR_NAME,
+        message: errorsMessage.RESTORE_NOT_CREATED_MESSAGE,
+        cause: errorsCause.RESTORE_NOT_CREATED_CAUSE,
+      })
+    );
   }
 }
 
-export async function changePassword(req, res) {
+export async function changePassword(req, res, next) {
   try {
     const { newPassword } = req.body;
     const { token } = req.query;
     const result = await restoreService.changePassword(token, newPassword);
 
-    return res.status(200).send(result);
+    logger.info("Password changed successfully.");
+
+    res.status(200).send(result);
   } catch (error) {
-    return res.status(500).send(error.message);
+    logger.error("Failed to change password.", error);
+    next(
+      new CustomError({
+        name: errorsName.CHANGE_PASSWORD_ERROR_NAME,
+        message: errorsMessage.CHANGE_PASSWORD_ERROR_MESSAGE,
+        cause: error,
+      })
+    );
   }
 }
